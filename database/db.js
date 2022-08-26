2f4bdcfc16b923337a3144cd85c4e5ed2117e97d
zoran <x00160216@mytudublin.ie>
2022-08-26 23:17:43 +0100

    new dep

--- database/db.js
+++ database/db.js
@@ -5,7 +5,7 @@ dotenv.config({path: "./vars/.env"})
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.CONNECT_DB, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+        await mongoose.connect("mongodb+srv://ZokiPM:AD8GJb9JoNRHf9ra@cluster0.ldxqb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
         console.log("Connected to MongoDB...");
     } catch (error) {
         console.log(error.message);
