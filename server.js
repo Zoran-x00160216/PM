bf97adeee2d6f3270ea7747144b8b60477091dbd
zoran <x00160216@mytudublin.ie>
2022-08-27 02:42:30 +0100

    new dep

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.use("/api/admin", verifyAdmin, require("./routes/admin"));
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "client/build/index.html"))
   })
   }
 
